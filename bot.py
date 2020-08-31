628c7b1a7c0000a3b7494f224efa1121303c06a9
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-08-31 14:56:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def handle_text(message):
             for i in range(150):
                 for k, v in rep.items():
                     if i == int(k):
-                        bot.send_message(message.chat.id, "ДЕНЬ " + str(i) + ":\n" + "полив: " + rep.get(str(k))[0] + " л.\n" + rep.get(str(k))[1])
+                        bot.send_message(message.chat.id, "ДЕНЬ " + str(i) + ":\n" + "полив: " + rep.get(str(k))[0] + " л.\n" + "описание: " + rep.get(str(k))[1])
                         
         except FileNotFoundError:
             bot.send_message(message.chat.id, "Записи отсутствуют.")
