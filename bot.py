cee32320d1598f68fb3d3f41e39245978d82c531
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-08-31 12:57:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,46 @@ import json
 bot = telebot.TeleBot("1346425132:AAHZfTwGvYBBDoJpRaFNMzqnXmNSuWxH6hg")
 filename = "nums.json"
 
+def report(text):
+    
+    s = text.split(",")
+    day = s[0]
+    water = s[1]
+    desc = s[2]
+    
+    try:
+        file = open(filename) 
+        rep = json.load(file)
+        lst = []
+        w = str(water) + ' litres'
+        lst = [w, desc]
+        rep[day] = lst
+        file.close()
+        
+    except FileNotFoundError:
+        rep = {}
+        file = open(filename, 'w')
+        lst = []
+        w = str(water) + ' litres'
+        lst = [w, desc]
+        rep[day] = lst
+        file.close()
+    
+    filea = open(filename, 'w')
+    json.dump(rep, filea)
+    filea.close()
+    
+    
+def out():
+    
+    rep = json.load(open(filename))
+    
+    for i in range(100):
+        for k, v in rep.items():
+            if i == int(k):
+                print("day " + str(i) + ": ")
+                print(rep.get(str(k)))
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -16,28 +56,12 @@ def handle_start(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
         try:
-            
-            file = open(filename) 
-            rep = json.load(file)
-            lst = []
-            w = str(water) + ' litres'
-            lst = [w, desc]
-            rep[day] = lst
-            file.close()
-            
-            bot.send_message(message.chat.id, "Данные добавлены.")   
+                report(message.text)
+                
+                bot.send_message(message.chat.id, "Данные добавлены.")   
             
         except:
-            
-            rep = {}
-            file = open(filename, 'w')
-            lst = []
-            w = str(water) + ' litres'
-            lst = [w, desc]
-            rep[day] = lst
-            file.close()
-            
-            bot.send_message(message.chat.id, "Данные добавлены.")
+                bot.send_message(message.chat.id, "Ошибка...")
              
         
 bot.polling(none_stop=True, interval=0)    
